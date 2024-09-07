bdbcc91e5b4dc7c8de87bf6844105ebf53741398
Charith Kaduruwewa <charithkaduru@gmail.com>
2024-09-07 12:31:13 +0530

    Initial commit with backend

---
+++ backend/src/test/java/com/abcrest/abcRestaurant/AbcRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abcrest.abcRestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
